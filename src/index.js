c0e07a6ce85e40d04e174f363f57417080f7b8c1
Rithz <101114513+RithzWang@users.noreply.github.com>
2024-06-03 07:41:29 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const { Client, Partials, Collection } = require('discord.js');
 const colors = require('colors');
 const config = require('./config/config.json')
 
-const keep_alive = require('./keep_alive.js')
 
 const client = new Client({
     intents: [
